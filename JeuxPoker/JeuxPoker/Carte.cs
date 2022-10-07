3a0287d08cac0b7c5491276d0a3d355fe26a0dc7
Lucas Derou <2032998@etu.cchic.ca>
2022-10-07 14:34:09 -0400

    Ajout Tour

--- JeuxPoker/JeuxPoker/Carte.cs
+++ JeuxPoker/JeuxPoker/Carte.cs
@@ -8,5 +8,12 @@ namespace JeuxPoker
 {
     internal class Carte
     {
+        // Chiffre 0 pour 2 et 12 pour As
+        // Couleur 0 pour Coeur , 1 pour Carreau , 2 pour Trefle , 3 pour Pique.
+        int[,] infoCarte;
+        public Carte(int[,] infoCarte)
+        {
+            this.infoCarte = infoCarte;
+        }
     }
 }
--- JeuxPoker/JeuxPoker/Paquet.cs
+++ JeuxPoker/JeuxPoker/Paquet.cs
@@ -7,6 +7,35 @@ using System.Threading.Tasks;
 namespace JeuxPoker
 {
     internal class Paquet
+
+    // Infos Cartes
+    // Chiffre 0 pour 2 et 12 pour As
+    // Couleur 0 pour Coeur , 1 pour Carreau , 2 pour Trefle , 3 pour Pique.
+
     {
+        Carte[] TableauInitial = new Carte[52];
+        Carte[] TableauTampon = new Carte[52];
+        List<int> TableauTamponList = new List<int>();
+        public Paquet()
+        {
+
+        }
+
+        public void Distribuer(Joueur j)
+        {
+        }
+        public void Melanger()
+        {
+        }
+        public void Reinitialiser()
+        {
+            for (int i = 0; i < 13; i++)
+            {
+
+            }
+        }
+        public void FlopTurnRiver(Tour t)
+        {
+        }
     }
 }
