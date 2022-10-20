4fc7c4b71507c164fec6c899a29a130be9d78a62
Nataniel simard <2132469@etu.cchic.ca>
2022-10-20 11:11:27 -0400

    Tour Completer

--- JeuxPoker/JeuxPoker/Tour.cs
+++ JeuxPoker/JeuxPoker/Tour.cs
@@ -8,5 +8,25 @@ namespace JeuxPoker
 {
     internal class Tour
     {
+        Carte[] carteCommune;
+        int etatTour;
+
+        Tour()
+        {
+            etatTour = 0;
+            carteCommune = new Carte[5];
+        }
+        public void changerEtat()
+        {
+            if (etatTour <=5)
+            {
+                carteCommune[etatTour].Retourner();
+                etatTour++;
+            }
+        }
+        public void ResetTour()
+        {
+            etatTour = 0;
+        }
     }
 }
